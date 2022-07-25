49924763a6da9a4de7d85806a587d3ae9c7959f9
Mihir <savla.mp@somaiya.edu>
2022-07-25 23:56:19 +0530

    first commit

---
+++ js/client.js
@@ -0,0 +1,4 @@
+const socket = io('http://localhost:3000')
+const form = document.getElementById('messageform')
+const messagebox = document.getElementById('text-box')
+const container = document.querySelector('.text-area')
